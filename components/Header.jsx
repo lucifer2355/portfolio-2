3884467e5a88c5d3591b8f6d4ce6d7d70f913506
“lucifer2355” <dgajjar999@gmail.com>
2021-05-30 13:32:18 +0530

    add custom font

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import Image from "next/image";
 import Drawer from "rc-drawer";
 
 const Header = () => {
   return (
     <Drawer open={true}>
-      <div></div>
-
       <div>
+        <Image
+          layout='fixed'
+          src={require("../assets/images/me.png")}
+          height={130}
+          width={130}
+          className='rounded-full overflow-hidden'
+        />
+      </div>
+
+      <div className='font-nunito'>
         <p>Home</p>
         <p>About</p>
         <p>Resume</p>
