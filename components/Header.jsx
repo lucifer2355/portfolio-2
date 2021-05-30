978b762e950ab8dcdd85fa43ee0e94f832b9d934
“lucifer2355” <dgajjar999@gmail.com>
2021-05-30 15:42:33 +0530

    add heroicons

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,28 +1,40 @@
 import React from "react";
 import Image from "next/image";
 import Drawer from "rc-drawer";
+import { MenuIcon } from "@heroicons/react/outline";
 
 const Header = () => {
   return (
-    <Drawer open={true}>
-      <div>
-        <Image
-          layout='fixed'
-          src={require("../assets/images/me.png")}
-          height={130}
-          width={130}
-          className='rounded-full overflow-hidden'
-        />
-      </div>
+    <>
+      <Drawer
+        width={"320px"}
+        open={false}
+        height={"100vh"}
+        duration={"0.3s"}
+        showMask={false}
+        level='null'
+      >
+        <div>
+          <Image
+            layout='fixed'
+            src={require("../assets/images/me.png")}
+            height={130}
+            width={130}
+            className='rounded-full overflow-hidden'
+          />
+        </div>
 
-      <div className='font-nunito'>
-        <p>Home</p>
-        <p>About</p>
-        <p>Resume</p>
-        <p>Portfolios</p>
-        <p>Contact</p>
-      </div>
-    </Drawer>
+        <div>
+          <p>Home</p>
+          <p>About</p>
+          <p>Resume</p>
+          <p>Portfolios</p>
+          <p>Contact</p>
+        </div>
+      </Drawer>
+
+      <div></div>
+    </>
   );
 };
 
