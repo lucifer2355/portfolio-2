c938139a3fa53c981b45850d37105a2e15df3784
“lucifer2355” <dgajjar999@gmail.com>
2021-05-30 22:50:18 +0530

    add border for image

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,17 +14,19 @@ const Header = ({ open, drawerToggle, drawerHandler }) => {
         handler={false}
         level={null}
         showMask={false}
-        className='xl:hidden'
+        className='xl:hidden focus:outline-none'
       >
-        <div className='border-r-2 w-60 h-screen border-gray-400 flex flex-col items-center'>
-          <div>
-            <Image
-              layout='fixed'
-              src={require("../assets/images/me.png")}
-              height={130}
-              width={130}
-              className='rounded-full overflow-hidden'
-            />
+        <div className='border-r-2 w-60 h-screen border-gray-400 border-opacity-20 flex flex-col items-center'>
+          <div className='mt-10 mb-10 pb-10 w-full border-b-2 border-gray-400 border-opacity-20 flex  justify-center'>
+            <div className='border-gray-400 border-4 border-opacity-10 rounded-full p-1 pb-0'>
+              <Image
+                layout='fixed'
+                src={require("../assets/images/me.png")}
+                height={130}
+                width={130}
+                className='rounded-full overflow-hidden'
+              />
+            </div>
           </div>
 
           <div>
