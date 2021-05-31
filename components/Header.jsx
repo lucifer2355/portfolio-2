1c6f7352f41d5ff3e869eff0077096ebd81fd681
“lucifer2355” <dgajjar999@gmail.com>
2021-05-31 11:32:16 +0530

    add hover effect in menu item

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,24 +17,28 @@ const Header = ({ open, drawerToggle, drawerHandler }) => {
         className='xl:hidden focus:outline-none'
       >
         <div className='border-r-2 w-60 h-screen border-gray-400 border-opacity-20 flex flex-col items-center'>
-          <div className='mt-10 mb-10 pb-10 w-full border-b-2 border-gray-400 border-opacity-20 flex  justify-center'>
+          <div className='mt-10 mb-16 pb-8 w-full border-b-2 border-gray-400 border-opacity-20 flex  justify-center'>
             <div className='border-gray-400 border-4 border-opacity-10 rounded-full p-1 pb-0'>
               <Image
                 layout='fixed'
                 src={require("../assets/images/me.png")}
                 height={130}
                 width={130}
-                className='rounded-full overflow-hidden'
+                className='rounded-full'
               />
             </div>
           </div>
 
-          <div>
-            <p>Home</p>
-            <p>About</p>
-            <p>Resume</p>
-            <p>Portfolios</p>
-            <p>Contact</p>
+          <div className='w-full h-52 justify-between text-lg uppercase font-thin text-gray-300 cursor-pointer inline-block'>
+            <ul className='flex flex-col text-justify h-52 justify-between w-full'>
+              <li className='text-center hover:bg-blue-400 hover:bg-opacity-20 hover:text-opacity-100 hover:text-blue-500'>
+                Home
+              </li>
+              <li>About</li>
+              <li>Resume</li>
+              <li>Portfolio</li>
+              <li>Contact</li>
+            </ul>
           </div>
         </div>
       </RcDrawer>
