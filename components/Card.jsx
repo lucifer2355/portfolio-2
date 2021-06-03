cdbe0cffeff9b5fbf5ed60323f3a8d407aa9104a
“lucifer2355” <dgajjar999@gmail.com>
2021-06-03 12:43:16 +0530

    change card border color on hover

--- components/Card.jsx
+++ components/Card.jsx
@@ -28,9 +28,9 @@ const services = [
 
 const Card = () => {
   return (
-    <div className='mt-8'>
+    <div className='mt-10'>
       {services.map((service) => (
-        <div className='mt-4 border border-[#2E344E] bg-[#191D2B] rounded-sm pr-5 pl-5 pt-5 pb-5'>
+        <div className='mt-6 border border-[#2E344E] border-t-4 bg-[#191D2B]  rounded-sm pr-5 pl-5 pt-5 pb-5 hover:border-blue-600'>
           <Image src={service.icon} width={40} height={40} color='text-white' />
           <h1 className='text-xl mt-5'>{service.title}</h1>
           <div className='mt-4 mb-8  after:rounded-sm after:active:bg-[#2E344E] after:bg-opacity-60 after:active:empty-content after:absolute after:left-4 after:h-0.5 after:w-14 after:left-10' />
