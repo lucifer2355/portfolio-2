5b266f35ba0a14cabf4aae7d30ce11d3f98156ab
“lucifer2355” <dgajjar999@gmail.com>
2021-06-03 22:28:52 +0530

    add personal data

--- components/PersonalInfo.jsx
+++ components/PersonalInfo.jsx
@@ -1,25 +1,17 @@
 import React from "react";
 
-const personalInfo = {
-  "Full Name": "Dhruvil Gajjar",
-  Age: "22 Years",
-  Degree: "B.Tech (Information Technology)",
-  Nationality: "Indian",
-  Languages: "English, Hindi, Gujarati",
-  Address: "Ahmedabad, Gujarat, India",
-  Freelance: "Available",
-};
+import { personalData } from "../data/personal/personal";
 
 const PersonalInfo = () => {
   return (
     <div className='mt-4'>
-      {Object.keys(personalInfo).map(function(key, index) {
+      {Object.keys(personalData).map(function(key, index) {
         return (
           <div key={index} className='text-gray-400 flex'>
             <span className='flex w-24 justify-between'>
               {key} <span>:</span>
             </span>
-            <span className='ml-2'>{personalInfo[key]}</span>
+            <span className='ml-2'>{personalData[key]}</span>
           </div>
         );
       })}
---
+++ data/personal/personal.js
@@ -0,0 +1,9 @@
+export const personalData = Object.freeze({
+  "Full Name": "Dhruvil Gajjar",
+  Age: "22 Years",
+  Degree: "B.Tech (Information Technology)",
+  Nationality: "Indian",
+  Languages: "English, Hindi, Gujarati",
+  Address: "Ahmedabad, Gujarat, India",
+  Freelance: "Available",
+});
