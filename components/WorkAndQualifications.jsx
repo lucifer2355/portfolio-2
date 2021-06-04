b6c765f402c4f44522c0ba481e503a43e549949b
“lucifer2355” <dgajjar999@gmail.com>
2021-06-04 12:08:07 +0530

    add work text style

--- components/WorkAndQualifications.jsx
+++ components/WorkAndQualifications.jsx
@@ -10,10 +10,12 @@ const WorkAndQualifications = () => {
       </div>
       <div className='border-l-2 pl-4 ml-1 mt-5 border-gray-200 h-auto'>
         <div className='w-11/12 flex flex-col'>
-          <span className=''>2018 - Present</span>
-          <span className=''>Frontend Web Developer</span>
-          <span className=''>abc company</span>
-          <p className='text-justify'>
+          <span className='text-gray-400 text-lg'>2018 - Present</span>
+          <span className='text-xl text-blue-500 text-opacity-100 font-bold'>
+            Frontend Web Developer
+          </span>
+          <span className='text-lg text-white font-thin'>abc company</span>
+          <p className='text-justify text-gray-400 mt-3'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ullam
             sed aspernatur hic cum magni minus explicabo commodi, consequatur
             quidem non at voluptas nisi sit.
