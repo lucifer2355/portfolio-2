23709db0bcc2856e58efb7df1940eba36b814546
“lucifer2355” <dgajjar999@gmail.com>
2021-06-04 12:01:55 +0530

    add work data html structure

--- components/WorkAndQualifications.jsx
+++ components/WorkAndQualifications.jsx
@@ -8,6 +8,18 @@ const WorkAndQualifications = () => {
         <BriefcaseIcon height={30} width={30} color='#ccc' />
         <span className='ml-3 text-2xl font-bold'>Working Experience</span>
       </div>
+      <div className='border-l-2 pl-4 ml-1 mt-5 border-gray-200 h-auto'>
+        <div className='w-11/12 flex flex-col'>
+          <span className=''>2018 - Present</span>
+          <span className=''>Frontend Web Developer</span>
+          <span className=''>abc company</span>
+          <p className='text-justify'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ullam
+            sed aspernatur hic cum magni minus explicabo commodi, consequatur
+            quidem non at voluptas nisi sit.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
