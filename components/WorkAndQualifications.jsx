d54e86c267d525897600c1988a8db46836e6f01f
“lucifer2355” <dgajjar999@gmail.com>
2021-06-04 12:43:26 +0530

    add work expreience data

--- components/WorkAndQualifications.jsx
+++ components/WorkAndQualifications.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BriefcaseIcon } from "@heroicons/react/outline";
 
+import { workExperiences } from "../data/resume/workExperiences";
+
 const WorkAndQualifications = () => {
   return (
     <div className='mt-10'>
@@ -8,21 +10,23 @@ const WorkAndQualifications = () => {
         <BriefcaseIcon height={30} width={30} color='#ccc' />
         <span className='ml-3 text-2xl font-bold'>Working Experience</span>
       </div>
-      <div className='border-l-2 pl-4 ml-1 mt-5 border-gray-200 h-auto relative'>
-        <div className='w-11/12 flex flex-col '>
-          <span className='text-gray-400 text-lg before:empty-content before:bg-[#10121B] before:h-4 before:w-4 before:rounded-xl before:absolute before:-left-2.5 before:top-2 before:border-4 before:border-gray-400'>
-            2018 - Present
-          </span>
-          <span className='text-xl text-blue-500 text-opacity-100 font-bold'>
-            Frontend Web Developer
-          </span>
-          <span className='text-lg text-white font-thin'>abc company</span>
-          <p className='text-justify text-gray-400 mt-3'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ullam
-            sed aspernatur hic cum magni minus explicabo commodi, consequatur
-            quidem non at voluptas nisi sit.
-          </p>
-        </div>
+      <div className='border-l-4 pl-4 ml-2 mt-5 border-[#2E344E] h-auto'>
+        {workExperiences.map((workExperience) => (
+          <div className='w-11/12 mt-5 flex flex-col relative'>
+            <span className='text-gray-400 text-lg before:empty-content before:bg-[#10121B] before:h-4 before:w-4 before:rounded-xl before:absolute before:-left-6 before:top-1.5 before:border-4 before:border-[#2E344E]'>
+              {workExperience.timeLine}
+            </span>
+            <span className='text-xl text-blue-500 text-opacity-100 font-bold'>
+              {workExperience.jobTitle}
+            </span>
+            <span className='text-lg text-white font-thin'>
+              {workExperience.companyName}
+            </span>
+            <p className='text-justify text-gray-400 mt-3'>
+              {workExperience.description}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ data/resume/workExperiences.js
@@ -0,0 +1,16 @@
+export const workExperiences = [
+  {
+    timeLine: "2020 - Present",
+    jobTitle: "React & React Native Developer",
+    companyName: "Codrain Technolabs",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ullam sed aspernatur hic cum magni minus explicabo commodi, consequatur quidem non at voluptas nisi sit.",
+  },
+  {
+    timeLine: "2019 - 2020",
+    jobTitle: "Magento Developer",
+    companyName: "Codrain Technolabs",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ullam sed aspernatur hic cum magni minus explicabo commodi, consequatur quidem non at voluptas nisi sit.",
+  },
+];
