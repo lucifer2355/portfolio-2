559288162ab570c214b34152f5aa9fdcbb4150d5
“lucifer2355” <dgajjar999@gmail.com>
2021-06-07 22:55:54 +0530

    add projects images

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+
 import MobileDrawer from "../components/MobileDrawer";
+import Title from "../components/Title";
 
 const portfolio = () => {
   return (
     <>
       <MobileDrawer />
-      <div className='pl-4 pr-4 pt-1 mb-10'></div>
+      <div className='pl-4 pr-4 pt-1 mb-10'>
+        <Title title='PORTFOLIOS' />
+      </div>
     </>
   );
 };
