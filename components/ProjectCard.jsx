2a6e51560a0d5905c24ced9616422b5b0d2cc785
“lucifer2355” <dgajjar999@gmail.com>
2021-06-08 16:01:30 +0530

    add portfolio card design

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const ProjectCard = () => {
   return (
-    <div className='w-11/12'>
-      <Image
-        src={require("../assets/projects/natours.png")}
-        height={300}
-        className='w-11/12'
-      />
-    </div>
+    <Link href='https://toursbook.netlify.app/' className='mt-10'>
+      <>
+        <Image
+          src={require("../assets/projects/natours.png")}
+          height={80}
+          width={130}
+          layout='responsive'
+          className='w-10/12'
+        />
+        <h1 className='text-2xl mt-2'>Natours</h1>
+        <h1 className='text-lg text-gray-400'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi sed
+          ducimus asperiores, iure sapiente odio aut odit nulla et vel quod
+          minus illo? Corporis, in.
+        </h1>
+      </>
+    </Link>
   );
 };
 
---
+++ data/portfolio/projects.js
@@ -0,0 +1,84 @@
+export const projects = [
+  {
+    title: "Phonix",
+    subtitle: "Video Streaming Application",
+    image: "/images/projects/phonix.png",
+    date: "2020-11",
+    desc:
+      "Phonix is a video streaming cross-platform mobile application. User can stream their favorit shows & movies. User also can download it",
+  },
+  {
+    title: "AI News",
+    subtitle: "ai news website",
+    link: "https://geekybot.com/",
+    image: "/images/projects/ai-news.png",
+    date: "2020-08-8",
+    desc:
+      "Voice command base NEWS web app. " +
+      "It's work only on voice command, " +
+      "Like `Give me the latest NEWS`, " +
+      "for AI part I'm using Alan AI and for frontend React.js & Metrial UI.",
+  },
+  {
+    title: "walkMapper",
+    subtitle: "New York Government Application",
+    // link: "https://devpost.com/software/harvest",
+    image: "/images/projects/walkMapper.png",
+    date: "2020-05-20",
+    desc:
+      "This is cross platform mobile application build in react native. " +
+      "Reason behind making this app is people send the road, street light, crossing problem to government, " +
+      "and government also store the problem.",
+  },
+  {
+    title: "Done With It",
+    subtitle: "New York Government Application",
+    link: "https://github.com/lucifer2355/pinterest",
+    image: "/images/projects/donewithit.png",
+    date: "2020-08-25",
+    desc:
+      "donw with it is part of learning React Native advance concepts. " +
+      "This app is for selling your old or unused things.",
+  },
+  {
+    title: "Khelmanjari",
+    subtitle: "2019/20 College spots event",
+    link: "http://khelmanjari.gmit.edu.in/",
+    image: "/images/projects/khelmanjari.png",
+    date: "2019-02-8",
+    desc:
+      "Craete this website for a college spots event. " +
+      "Student can register for spots, " +
+      "This site is work as portal. In this site show live scrore, spots schedule, game rules.",
+  },
+  {
+    title: "Natours",
+    subtitle: "Natours",
+    link: "https://toursbook.netlify.app/",
+    image: "/images/projects/natours.png",
+    date: "2020-08-10",
+    desc:
+      "This is single page wesite. It's a tour booking related website design. I make this website using react.js + SCSS. This website is part of learning SASS & React",
+  },
+  {
+    title: "Shopping Application",
+    subtitle: "Basic shopping application.",
+    link: "https://github.com/lucifer2355/shop-app",
+    image: "/images/projects/shopApp.png",
+    date: "2020-01-17",
+    desc:
+      "This is cross plateform mobile application build in react native. " +
+      "Application is basic shopping app. Use can login and register, add product. " +
+      "Cart option is also available .",
+  },
+  {
+    title: "To Do List",
+    subtitle: "Task Manager Application",
+    link: "https://github.com/lucifer2355/toDoList",
+    image: "/images/projects/todolist.jpg",
+    date: "2020-06-21",
+    desc:
+      "This is task manager application. " +
+      "Maintain your daily task and remind you task on time. Show completed and next task list. ",
+  },
+];
