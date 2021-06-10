98b8faaabf3eb087f9a2d46902bdf0538a830364
“lucifer2355” <dgajjar999@gmail.com>
2021-06-10 22:53:30 +0530

    add contact form state

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -3,21 +3,12 @@ import React, { useState } from "react";
 import MobileDrawer from "../components/MobileDrawer";
 import Title from "../components/Title";
 
-const styles = (theme) => ({
-  textField: {
-    width: "90%",
-    marginLeft: "auto",
-    marginRight: "auto",
-    paddingBottom: 0,
-    marginTop: 0,
-    fontWeight: 500,
-  },
-  input: {
-    color: "white",
-  },
-});
-
 const contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <>
       <MobileDrawer />
@@ -27,31 +18,42 @@ const contact = () => {
         <div className='mt-10'>
           <h1 className='text-2xl font-extralight'>Get In Touch</h1>
 
-          <div>
+          <div className='w-12/12'>
             <input
               type='text'
               name='name'
               placeholder='Enter Your Name'
-              className='bg-[#10121B] text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-11/12 mt-5 placeholder:text-gray-300'
+              className='bg-[#10121B] text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-full mt-5 placeholder:text-gray-300'
+            />
+            <input
+              type='email'
+              name='name'
+              placeholder='Enter Your Email'
+              className='bg-[#10121B] text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-full mt-5 placeholder:text-gray-300'
             />
             <input
               type='text'
               name='name'
-              placeholder='Enter Your Name'
-              className='bg-[#10121B] text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-11/12 mt-5 placeholder:text-gray-300'
+              placeholder='Enter Your Subject'
+              className='bg-[#10121B] text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-full mt-5 placeholder:text-gray-300'
             />
-            <input
+            <textarea
               type='text'
               name='name'
-              placeholder='Enter Your Name'
-              className='bg-[#10121B] text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-11/12 mt-5 placeholder:text-gray-300'
+              rows={5}
+              placeholder='Enter Your Message'
+              className='bg-[#10121B] text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-full mt-5 placeholder:text-gray-300'
             />
-            <input
-              type='text'
-              name='name'
-              placeholder='Enter Your Name'
-              className='bg-[#10121B] text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-11/12 mt-5 placeholder:text-gray-300'
-            />
+          </div>
+
+          <div className='mt-4'>
+            <button
+              className='bg-blue-600 hover:bg-blue-800 focus:outline-none transform active:translate-y-1 rounded-md pt-2 pb-2 pr-3 pl-3 font-extralight'
+              type='submit'
+              onClick={() => console.log("send mail")}
+            >
+              SEND MAIL
+            </button>
           </div>
         </div>
       </div>
