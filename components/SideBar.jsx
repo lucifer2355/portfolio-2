f9f3439143092b79349b446638f144a6a6dbc392
“lucifer2355” <dgajjar999@gmail.com>
2021-06-17 21:45:10 +0530

    add navlinks data

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 const SideBar = () => {
   return (
     <div className='bg-[#191D2B] w-3/12 border-r-2 border-gray-400 border-opacity-20'>
-      <div className='flex justify-center mt-8'>
+      {/* Profile Image */}
+      <div className='flex justify-center mt-8 border-b-2 border-gray-400 border-opacity-20 pb-5'>
         <div className='border-4 border-gray-400 border-opacity-20 rounded-full w-8/12 flex justify-center items-center'>
           <Image
             src={require("../assets/images/me.png")}
@@ -14,6 +15,12 @@ const SideBar = () => {
           />
         </div>
       </div>
+
+      {/* Nav Links */}
+      <div></div>
+
+      {/* Social Link */}
+      <div></div>
     </div>
   );
 };
---
+++ data/navlinks/navlinks.js
@@ -0,0 +1,22 @@
+export const navlinks = [
+  {
+    title: "Home",
+    link: "/",
+  },
+  {
+    title: "About",
+    link: "/about",
+  },
+  {
+    title: "Resume",
+    link: "/resume",
+  },
+  {
+    title: "Portfolio",
+    link: "/portfolio",
+  },
+  {
+    title: "Contact",
+    link: "/contact",
+  },
+];
