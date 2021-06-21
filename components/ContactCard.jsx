0376f24bc1a390d101c94f51119e19a9082e5948
“lucifer2355” <dgajjar999@gmail.com>
2021-06-21 10:57:37 +0530

    add contact card style for xl screen

--- components/ContactCard.jsx
+++ components/ContactCard.jsx
@@ -2,18 +2,21 @@ import React from "react";
 
 const ContactCard = ({ Icon, title, contactString }) => {
   return (
-    <div className='bg-[#191D2B] py-5 px-5 mt-5'>
+    <div className='bg-[#191D2B] py-5 px-5 mt-5 xl:flex'>
       <div className='border border-[#A4ACC4] border-opacity-30 w-14 h-14 flex items-center justify-center'>
         {Icon}
       </div>
-      <h1 className='mt-2 text-lg'>{title}</h1>
 
-      <div className='flex flex-col mt-2 h-12 justify-between text-gray-400'>
-        {contactString.map((string) => {
-          if (title === "Email")
-            return <a href={`mailto:${string}`}>{string}</a>;
-          else return <span>{string}</span>;
-        })}
+      <div className='xl:pl-5'>
+        <h1 className='mt-2 text-lg xl:text-2xl xl:mt-0'>{title}</h1>
+
+        <div className='flex flex-col mt-2 h-12 justify-between text-gray-400 xl:text-lg'>
+          {contactString.map((string) => {
+            if (title === "Email")
+              return <a href={`mailto:${string}`}>{string}</a>;
+            else return <span>{string}</span>;
+          })}
+        </div>
       </div>
     </div>
   );
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import MobileDrawer from "../components/MobileDrawer";
+import SideBar from "../components/SideBar";
 import Title from "../components/Title";
 import ContactCard from "../components/ContactCard";
 import { contacts } from "../data/contact/contact";
@@ -12,57 +13,63 @@ const contact = () => {
   const [message, setMessage] = useState("");
 
   return (
-    <>
+    <div className='flex'>
       <MobileDrawer />
-      <div className='px-4 pt-1 mb-10'>
+      <SideBar />
+
+      <div className='flex flex-col xl:w-5/6 xl:px-10 px-4 pt-1 mb-10'>
         <Title title='CONTACT ME' />
 
-        <div className='mt-10'>
-          <h1 className='text-2xl font-extralight'>Get In Touch</h1>
+        <div className='mt-10 flex space-x-4'>
+          <div className='w-7/12'>
+            <h1 className='text-2xl font-extralight xl:text-3xl'>
+              Get In Touch
+            </h1>
 
-          <div className='w-12/12'>
-            <input
-              type='text'
-              name='name'
-              placeholder='Enter Your Name'
-              className='bg-[#10121B] border-gray-400 text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-full mt-5 placeholder:text-gray-300'
-              onChange={(e) => setName(e.target.value)}
-            />
-            <input
-              type='email'
-              name='email'
-              placeholder='Enter Your Email'
-              className='bg-[#10121B] border-gray-400 text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-full mt-5 placeholder:text-gray-300'
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type='text'
-              name='subject'
-              placeholder='Enter Your Subject'
-              className='bg-[#10121B] border-gray-400 text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-full mt-5 placeholder:text-gray-300'
-              onChange={(e) => setSubject(e.target.value)}
-            />
-            <textarea
-              type='text'
-              name='message'
-              rows={5}
-              placeholder='Enter Your Message'
-              className='bg-[#10121B] border-gray-400 text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-full mt-5 placeholder:text-gray-300'
-              onChange={(e) => setMessage(e.target.value)}
-            />
+            <div className='w-12/12'>
+              <input
+                type='text'
+                name='name'
+                placeholder='Enter Your Name'
+                className='bg-[#10121B] border-gray-400 text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-full mt-5 placeholder:text-gray-300'
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                type='email'
+                name='email'
+                placeholder='Enter Your Email'
+                className='bg-[#10121B] border-gray-400 text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-full mt-5 placeholder:text-gray-300'
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type='text'
+                name='subject'
+                placeholder='Enter Your Subject'
+                className='bg-[#10121B] border-gray-400 text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-full mt-5 placeholder:text-gray-300'
+                onChange={(e) => setSubject(e.target.value)}
+              />
+              <textarea
+                type='text'
+                name='message'
+                rows={5}
+                placeholder='Enter Your Message'
+                className='bg-[#10121B] border-gray-400 text-gray-300 text-md font-medium border-2 rounded-lg pl-2 py-2 focus:outline-none w-full mt-5 placeholder:text-gray-300'
+                onChange={(e) => setMessage(e.target.value)}
+              />
+            </div>
+
+            <div className='mt-4'>
+              <button
+                className='bg-blue-600 hover:bg-blue-800 focus:outline-none transform active:translate-y-1 rounded-md pt-2 pb-2 pr-3 pl-3 font-extralight'
+                type='submit'
+                onClick={() => console.log("send mail")}
+              >
+                SEND MAIL
+              </button>
+            </div>
           </div>
 
-          <div className='mt-4'>
-            <button
-              className='bg-blue-600 hover:bg-blue-800 focus:outline-none transform active:translate-y-1 rounded-md pt-2 pb-2 pr-3 pl-3 font-extralight'
-              type='submit'
-              onClick={() => console.log("send mail")}
-            >
-              SEND MAIL
-            </button>
-          </div>
-
-          <div>
+          <div className='xl:w-2/5 xl:flex xl:flex-col justify-center xl:mb-6'>
             {contacts.map((contact) => (
               <ContactCard
                 Icon={contact.Icon}
@@ -73,7 +80,7 @@ const contact = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
